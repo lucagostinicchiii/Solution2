cbc057e45b0150439e1d9d58b054d6eba284aff8
gosti <gosti@LAPTOP-RSCNUK9K>
2020-11-16 10:57:42 +0100

    AsciiCode: creazione del progetto

--- AsciiCode/Program.cs
+++ AsciiCode/Program.cs
@@ -6,7 +6,7 @@ namespace AsciiCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
         }
     }
 }
