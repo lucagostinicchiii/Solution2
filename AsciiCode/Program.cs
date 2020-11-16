223a8805c8ee3b5d2a6178eb922137bec05f4848
gosti <gosti@LAPTOP-RSCNUK9K>
2020-11-16 10:56:04 +0100

    Aggiungere i file di progetto.

---
+++ AsciiCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsciiCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
